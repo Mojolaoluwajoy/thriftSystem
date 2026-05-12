fef365cfdec6199f0b60a4052921ce79e3204b5b
Mojolaoluwajoy <adebolamojolaoluwajoy@gmail.com>
2026-05-12 07:48:00 +0100

    add scheduler service

--- src/main/java/com/thrift/thriftsystem/service/SchedulerService.java
+++ src/main/java/com/thrift/thriftsystem/service/SchedulerService.java
@@ -1,4 +1,21 @@
 package com.thrift.thriftsystem.service;
 
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
 public class SchedulerService {
+
+    private final CurrencyService currencyService;
+
+    @Scheduled(cron = "0 0 0 * * *")
+    public void updateExchangeRates() {
+        log.info("Scheduled exchange rate update starting...");
+        currencyService.updateExchangeRates();
+        log.info("Scheduled exchange rate update completed.");
+    }
 }
